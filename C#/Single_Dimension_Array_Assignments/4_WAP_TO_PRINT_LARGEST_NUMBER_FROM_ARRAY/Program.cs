09950381793a3cb23f0c6cca8fa879144eaaa49c
Saqlain Sheikh <sheikhsaqlainsfdv@gmail.com>
2024-08-22 11:37:26 +0530

    commiting 4_WAP_TO_PRINT_LARGEST_NUMBER_FROM_ARRAY/

---
+++ C#/Single_Dimension_Array_Assignments/4_WAP_TO_PRINT_LARGEST_NUMBER_FROM_ARRAY/Program.cs
@@ -0,0 +1,65 @@
+﻿/*
+ 4. WAP to print the Largest Number from an Array. ( any onr may get asked-----> to find 2nd Largest or 3rd Largest or any  ).
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_WAP_TO_PRINT_LARGEST_NUMBER_FROM_ARRAY
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Array Size :-");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[size];
+            int max1, max2, max3;
+            Console.WriteLine("Enter Array Elements :-");
+            for (int position = 0; position < size; position++)
+            {
+                arr[position] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nThe Entered Array Elements are:-");
+            for (int position = 0; position < size; position++)
+            {
+                Console.Write(arr[position] + " ");
+            }
+            Console.WriteLine();
+
+            max1 = 0;
+            max2 = 0;
+            max3=0;
+            for (int count = 0; count < size; count++)
+            {
+                if (arr[count] > max1)
+                {
+                    max2 = max1;
+                    max1 = arr[count];
+                }
+                else if (arr[count] > max2)
+                {
+                    if (max1 == max2)
+                    {
+                        break;
+                    }
+                    max3 = max2;
+                    max2 = arr[count];
+                }
+                else if (arr[count] > max3)
+                {
+                    int tmp = max3;
+                    max3 = arr[count];
+                }
+            }
+            Console.WriteLine("Here 1st Largest Element in the Array is:-" + max1);  //First Largest.
+            Console.WriteLine("Here 2nd Largest Element in the Array is:-" + max2);  //First Largest.
+            Console.WriteLine("Here 3rd Largest Element in the Array is:-" + max3);  //First Largest.
+
+            Console.Read();
+        }
+    }
+}
---
+++ C#/Single_Dimension_Array_Assignments/4_WAP_TO_PRINT_LARGEST_NUMBER_FROM_ARRAY/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("4_WAP_TO_PRINT_LARGEST_NUMBER_FROM_ARRAY")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("4_WAP_TO_PRINT_LARGEST_NUMBER_FROM_ARRAY")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("692e5911-3729-4cdc-aa39-fb812afadbc4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
