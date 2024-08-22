9ef02f9106ad53bbb86cf9acdc2d2b0425cdb0f6
Saqlain Sheikh <sheikhsaqlainsfdv@gmail.com>
2024-08-22 11:32:19 +0530

    5_\ Display_The_Cube_Of_The_Number/

---
+++ C#/Loops_Assignments/5_ Display_The_Cube_Of_The_Number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5__Display_The_Cube_Of_The_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number To Calculate Its Cube:- ");
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+            int Cube = Number * Number * Number;
+
+            Console.WriteLine("Cube Of Number Is:- "+Cube);
+            Console.Read();
+        }
+    }
+}
