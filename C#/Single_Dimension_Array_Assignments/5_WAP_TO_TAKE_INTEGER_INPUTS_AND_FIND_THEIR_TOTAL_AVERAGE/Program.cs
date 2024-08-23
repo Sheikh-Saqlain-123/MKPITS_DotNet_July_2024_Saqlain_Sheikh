e28669fc5ade3a5db71f6ac3ecca66dad6ea3e1d
Saqlain Sheikh <sheikhsaqlainsfdv@gmail.com>
2024-08-23 15:26:07 +0530

    commiting 5_WAP_TO_TAKE_INTEGER_INPUTS_AND_FIND_THEIR_TOTAL_AVERAGE to C#/Single_Dimensional_Array_Assignments Folder

---
+++ C#/Single_Dimension_Array_Assignments/5_WAP_TO_TAKE_INTEGER_INPUTS_AND_FIND_THEIR_TOTAL_AVERAGE/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_WAP_TO_TAKE_INTEGER_INPUTS_AND_FIND_THEIR_TOTAL_AVERAGE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Array Size:- ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int average = 0;int total = 0;
+            int[] arr = new int[size];
+            Console.WriteLine("Enter Array Elements:- ");
+            for (int count=0; count<size; count++)
+            {
+                arr[count] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Enterd Elements in Array are :- ");
+            for (int count = 0; count < size; count++)
+            {
+                Console.WriteLine(arr[count]);
+                total = total + arr[count];
+            }
+            average = total / size;
+            Console.WriteLine("Abverage of teh Elements of the Array is:- "+average);
+
+            Console.Read();
+        }
+    }
+}
