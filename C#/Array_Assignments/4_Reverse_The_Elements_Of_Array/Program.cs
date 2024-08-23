3bb2b2dc9e611a7a0e915922afd40a03cef273d6
Saqlain Sheikh <sheikhsaqlainsfdv@gmail.com>
2024-08-23 15:10:58 +0530

    commiting 4_Reverse_the_Element_of_Array Assignment in c#/Array_Assignments Folder

---
+++ C#/Array_Assignments/4_Reverse_The_Elements_Of_Array/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+4. Reverse Array:
+○ Write a C program to reverse the elements of a given array
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Reverse_The_Elements_Of_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Array Size :-");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[size];
+            for (int position = 0; position < size; position++)
+            {
+                arr[position] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Entered Array Elements Are :- ");
+            for (int position = 0; position < size; position++)
+            {
+                Console.Write(arr[position] + " ");
+
+            }
+            int startIndex = 0;
+            int endIndex = arr.Length - 1;
+
+            //while (startIndex<endIndex)
+            //{
+            //    int tmp = arr[startIndex];
+            //    arr[startIndex] = arr[endIndex];
+            //    arr[endIndex] = tmp;
+            //    startIndex++;
+            //    endIndex--;
+            //}
+
+            for (int cnt = 0; cnt < arr.Length; cnt++)
+            {
+                int temp = arr[cnt];
+                arr[cnt] = arr.Length - startIndex - 1;
+                arr[arr.Length - startIndex - 1] = temp;
+            }
+            Console.WriteLine("Array Elements in Reverse Order are:-");
+            for (int position = 0; position < size; position++)
+            {
+                Console.Write(arr[position] + " ");
+
+            }
+
+
+
+            Console.Read();
+        }
+    }
+}
