8350482397906ec64430a7450719c8f8eed97077
Saqlain Sheikh <sheikhsaqlainsfdv@gmail.com>
2024-08-27 10:53:22 +0530

    commiting Assignment 8 to C#/Loops_Assignments/

---
+++ C#/Loops_Assignments/9_Display_pattern_like_Right_angle_Triangle_using_Asterik _Sign/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Display_pattern_like_Right_angle_Triangle_using_Asterik__Sign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number OF Rows you want to print the Right Angle Triangle In :- ");
+            int numOfRows = Convert.ToInt32(Console.ReadLine());
+            for(int rows=0; rows < numOfRows; rows++)
+            {
+                for(int column=0; column < rows; column++)
+                {
+                    Console.Write("*  ");
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ C#/Loops_Assignments/9_Display_pattern_like_Right_angle_Triangle_using_Asterik _Sign/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("9_Display_pattern_like_Right_angle_Triangle_using_Asterik _Sign")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("9_Display_pattern_like_Right_angle_Triangle_using_Asterik _Sign")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("38ed06f8-58b5-4a4b-8f03-96747c5852d2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
