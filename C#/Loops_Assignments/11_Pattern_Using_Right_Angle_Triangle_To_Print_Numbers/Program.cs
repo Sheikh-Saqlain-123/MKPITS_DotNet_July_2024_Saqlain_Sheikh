7fc8bcdcc84093394560ca2bfe643e131fa8d612
Saqlain Sheikh <sheikhsaqlainsfdv@gmail.com>
2024-08-27 10:54:58 +0530

    commiting Assignment 11 to C#/Loops_Assignments/

---
+++ C#/Loops_Assignments/11_Pattern_Using_Right_Angle_Triangle_To_Print_Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿/* 11. Write a program in C to make such a pattern like a right angle triangle
+with a number which will repeat a number in a row.
+The pattern like :
+1
+22
+333
+4444
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Pattern_Using_Right_Angle_Triangle_To_Print_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int outercounter=1; outercounter<5; outercounter++)
+            {
+                for (int innercounter=0; innercounter<outercounter; innercounter++)
+                {
+                    Console.Write(outercounter+" ");
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
