1872c616f2060e3fc7ed033ca469db05849e8aa3
Saqlain Sheikh <sheikhsaqlainsfdv@gmail.com>
2024-08-28 17:27:45 +0530

    commiting Assignment 12 to C#/Loops_Assignments/

---
+++ C#/Loops_Assignments/12_Pattern_Using_Right_Angle_Triangle_To_Print_Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿/* 12. Write a program in C to make such a pattern like a right angle triangle
+with the number increased by 1.
+The pattern like :
+1
+2 3
+4 5 6
+7 8 9 10
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_Pattern_Using_Right_Angle_Triangle_To_Print_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Result = 1;
+            for (int outercounter=1; outercounter<=4;outercounter++)
+            {
+                for (int innercounter=1; innercounter<=outercounter; innercounter++)
+                {
+                    Console.Write((Result++)+" ");
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
