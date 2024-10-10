e8fc2f8a652bf78baeba47660cc85f691467fe4f
Saqlain Sheikh <sheikhsaqlainsfdv@gmail.com>
2024-10-10 10:17:30 +0530

    commiting OOP Assignment 11 to C#/Object_Oriented_Programming_Assignments Folder

---
+++ C#/Object_Oriented_Programming_Assignments/OOP_11_String_Concatenation_Using_Operator_Overloading/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_11_String_Concatenation_Using_Operator_Overloading
+{
+    class ConcatenateStrings
+    {
+        private string str;
+
+        // Constructor to initialize the string
+        public ConcatenateStrings(string str)
+        {
+            this.str = str;
+        }
+
+        // Overloading the + operator to concatenate two strings
+        public static ConcatenateStrings operator +(ConcatenateStrings s1, ConcatenateStrings s2)
+        {
+            return new ConcatenateStrings(s1.str + s2.str);
+        }
+
+        // Method to display the string
+        public void Display()
+        {
+            Console.WriteLine(str);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
