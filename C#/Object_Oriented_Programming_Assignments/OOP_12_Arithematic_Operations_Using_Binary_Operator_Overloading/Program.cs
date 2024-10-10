46c0a2e833cc970b1f2670522948f69ad95610c2
Saqlain Sheikh <sheikhsaqlainsfdv@gmail.com>
2024-10-10 10:17:55 +0530

    commiting OOP Assignment 12 to C#/Object_Oriented_Programming_Assignments Folder

---
+++ C#/Object_Oriented_Programming_Assignments/OOP_12_Arithematic_Operations_Using_Binary_Operator_Overloading/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_12_Arithematic_Operations_Using_Binary_Operator_Overloading
+{
+    class Numbers
+    {
+        private int value;
+
+        // Constructor to initialize the number
+        public Numbers(int value)
+        {
+            this.value = value;
+        }
+
+        // Overloading the + operator
+        public static Numbers operator +(Numbers n1, Numbers n2)
+        {
+            return new Numbers(n1.value + n2.value);
+        }
+
+        // Overloading the - operator
+        public static Numbers operator -(Numbers n1, Numbers n2)
+        {
+            return new Numbers(n1.value - n2.value);
+        }
+
+        // Overloading the * operator
+        public static Numbers operator *(Numbers n1, Numbers n2)
+        {
+            return new Numbers(n1.value * n2.value);
+        }
+
+        // Method to display the value
+        public void Display()
+        {
+            Console.WriteLine(value);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers num1 = new Numbers(10);
+            Numbers num2 = new Numbers(5);
+
+            Console.WriteLine("Choose an operation: +, -, *");
+            char operation = Console.ReadLine()[0];
+
+            Numbers result = null;
+
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation");
+                    return;
+            }
+
+            Console.WriteLine("Result of the operation:");
+            result.Display();
+
+            Console.Read();
+        }
+    }
+}
