afc6c89077ed44005fcc57f002bf52d9c26a7869
Saqlain Sheikh <sheikhsaqlainsfdv@gmail.com>
2024-10-10 10:15:05 +0530

    commiting OOP Assignment 8 to C#/Object_Oriented_Programming_Assignments Folder

---
+++ C#/Object_Oriented_Programming_Assignments/OOP_8_Multiplication_Cubic_Values_Using_Inline_Function/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_8_Multiplication_Cubic_Values_Using_Inline_Function
+{
+    public class InlineFunctionClass
+    {
+        public void InlineMultiplicationFunction(int number1, int number2)
+        {
+            Console.WriteLine("The Multiplication Of " + number1+" & "+number2+" is :-" + (number1 * number2));
+        }
+        public void InlineCubeFunction(decimal num)
+        {
+            Console.WriteLine("The Cube Of "+num+" is :-"+(num*num*num));
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InlineFunctionClass obj = new InlineFunctionClass();
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine("Enter The Two Numbers For Multiplication :-");
+            int num1= Convert.ToInt32(Console.ReadLine());
+            int num2= Convert.ToInt32(Console.ReadLine());
+            obj.InlineMultiplicationFunction(num1, num2);
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine("Enter The Numbers to find Its Cube :-");
+            decimal number= Convert.ToDecimal(Console.ReadLine());
+            obj.InlineCubeFunction(number);
+            Console.WriteLine("---------------------------------------------------------------------------");
+
+            Console.Read();
+        }
+    }
+}
